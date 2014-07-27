c154e824a31d553e7ebcfda60acdd6c4ee9bca59
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2014-07-27 16:33:33 +0200

    Update Shader.java

--- src/dwarf/engine/core/Shader.java
+++ src/dwarf/engine/core/Shader.java
@@ -4,9 +4,7 @@ import org.lwjgl.opengl.GL20.*;
 
 /**
  * handles all shader files and shader operation
- *
- * @author sid_th3_sl0th
  */
-public class Shader extends java.lang.Object {
-
+public class Shader {
+  //TODO
 }
