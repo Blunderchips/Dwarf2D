943a7bec32cc7ad6932da74ed4a7a2d71422d572
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2014-07-30 13:25:47 +0200

    Update Polygon.java

--- src/dwarf/graphics/Polygon.java
+++ src/dwarf/graphics/Polygon.java
@@ -5,7 +5,14 @@ import dwarf.util.Vector2;
 import java.util.Objects;
 
 /**
- * A simple wrapper round the values required for a mutable Polygon class
+ * A simple wrapper round the values required for a mutable Polygon class. <br/>
+ * In geometry a polygon is traditionally a plane figure that is
+ * bounded by a finite chain of straight line segments closing in a loop to form
+ * a closed chain or circuit. These segments are called its edges or sides, and
+ * the points where two edges meet are the polygon's vertices (singular: vertex)
+ * or corners. The interior of the polygon is sometimes called its body. An
+ * n-gon is a polygon with n sides. A polygon is a 2-dimensional example of the
+ * more general polytope in any number of dimensions.
  *
  * @author sid_th3_sl0th
  */
