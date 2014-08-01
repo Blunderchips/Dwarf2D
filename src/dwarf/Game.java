c3a72b60ab6270152ff9c8d1e39cfea0a0f5936f
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2014-08-01 22:05:05 +0200

    update

--- src/dwarf/Game.java
+++ src/dwarf/Game.java
@@ -19,7 +19,7 @@ import java.util.Objects;
  */
 public abstract class Game extends Engine {
 
-    public ArrayList<GameObject> gameObjects;
+    public static ArrayList<GameObject> gameObjects;
     public static boolean debug = true;
     //NetBeans wanted these:
     public static final String PROP_GAMEOBJECTS = "PROP_GAMEOBJECTS";
