7900ce8dd8991599a3ff34f45f29a32b63c3d4f4
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2014-08-04 13:33:46 +0200

    Update openAL.java

--- src/dwarf/engine/core/openAL.java
+++ src/dwarf/engine/core/openAL.java
@@ -40,7 +40,7 @@ public final class openAL {
     protected static void init() {
         try {
             AL.create();
-            //util.debug("INFO", "openAL version: " + openAL.getVersion());
+            // util.debug("INFO", "openAL version: " + openAL.getVersion());
         } catch (LWJGLException ex) {
             System.err.println(ex);
             JOptionPane.showMessageDialog(
