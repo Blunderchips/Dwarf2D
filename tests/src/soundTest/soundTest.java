56c3637b42656119dd8d9848b63b03dd3aa526fe
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2014-08-30 16:35:41 +0200

    update

---
+++ tests/src/soundTest/soundTest.java
@@ -0,0 +1,44 @@
+package soundTest;
+
+import dwarf.Sfx;
+import dwarf.Game;
+import dwarf.util.Vector2;
+import dwarf.DwarfException;
+
+import static dwarf.gfx.Colours.black;
+import static dwarf.gfx.Colours.white;
+import static dwarf.keyboard.isKeyDown;
+
+public class soundTest {
+
+    public static void main(String[] args) {
+        new Game("audio test - Dwarf 2D") {
+
+            private Sfx song;
+
+            @Override
+            public void load() {
+                dwarf.gfx.background.setColour(white);
+
+                try {
+                    this.song = new Sfx("./src/soundTest/sound.wav");
+                } catch (DwarfException ex) {
+                    ex.print();
+                }
+            }
+
+            @Override
+            public void update() {
+                if (isKeyDown("space")) {
+                    this.song.play();
+                }
+            }
+
+            @Override
+            public void render() {
+                dwarf.gfx.draw.basicText("Press the spacebar to the play sound.", new Vector2(250, 400), black);
+            }
+        };
+    }
+
+}
