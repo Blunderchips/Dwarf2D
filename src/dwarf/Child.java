725efa2ab7e1fff10ed6abe34e07e0cc24b67e1c
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2014-09-06 16:43:59 +0200

    Update Child.java
    added comments

--- src/dwarf/Child.java
+++ src/dwarf/Child.java
@@ -2,6 +2,13 @@ package dwarf;
 
 import java.util.ArrayList;
 
+/**
+ * Definition of anything that can have parent.
+ * 
+ * @author Matthew 'siD' Van der Bijl
+ * 
+ * @see dwarf.Parent
+ */
 public interface Child {
 
     public abstract Parent getParent();
@@ -15,7 +22,13 @@ public interface Child {
     public static boolean hasSiblings(Child child) {
         return child.getParent().getChildren().size() > 1;
     }
-
+    
+   /**
+    * returns true if the inputed <code>Child</code> has a <code>Parent</code> otherwise false.
+    *
+    * @param parent the <code>Child</code> to be tested
+    * @return if the child a has parent or not
+    */
     public static boolean hasParent(Child child) {
         try {
             child.getParent().getChildren();
