945e97bef315c68432443e374e6f23b4d897ee99
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2014-09-06 15:32:52 +0200

    Create package-info.java

---
+++ src/dwarf/LWJGL/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Holds all classes take from the Lightweight Java Game Library Project
+ * (LWJGL).
+ *
+ * @see dwarf.LWJGL.AiffData
+ * @see dwarf.LWJGL.Texture
+ * @see dwarf.LWJGL.TextureLoader
+ * @see dwarf.LWJGL.WaveData
+ */
+package dwarf.LWJGL;
